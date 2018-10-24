97c6d6f11826be950a044e375628cfa8709e4197
SapphireClawe <37085710+SapphireClawe@users.noreply.github.com>
2018-10-24 11:29:13 -0600

    e

---
+++ Pong_Multitude/Star.java
@@ -0,0 +1,23 @@
+class Star {
+  private float x;
+  private float y;
+  private float radius;
+  
+  public static int starCount = 1; 
+  public static int maxRadius = 20; 
+  public Star(float x, float y, float radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  } 
+
+  public float getX() {
+    return x;
+  }
+  public float getY() {
+    return y;
+  }
+  public float getRadius() {
+    return radius;
+  }
+}
